ded55c97c44bf7617035ef0a8383d0d718d3c310
Nefisa Brčaninović <nefisa.h@hotmail.com>
2018-10-17 17:07:42 +0200

    Poruka

---
+++ src/Konverter.java
@@ -0,0 +1,28 @@
+
+public class Konverter {
+
+	public double kilogramUFunte(double a) {
+		return 0;
+	}
+	
+	public double funteUKilograme(double a) {
+		return 0;
+	}
+	
+	public double cmUInce(double a) {
+		return 0;
+	}
+	
+	public double inceUCm(double a) {
+		return 0;
+	}
+	
+	public double celzijusiUFarenhajte(double a) {
+		return 0;
+	}
+	
+	public double farnehajtiUCelzijuse(double a) {
+		return 0;
+	}
+
+}
