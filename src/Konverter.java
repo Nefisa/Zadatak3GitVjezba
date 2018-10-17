832ecf90d4f36ca5d585f0006aa7a2b6e7146d2b
Nefisa Brčaninović <nefisa.h@hotmail.com>
2018-10-17 20:00:34 +0200

    Implementirano F to C

--- src/Konverter.java
+++ src/Konverter.java
@@ -4,25 +4,25 @@ public class Konverter {
 	public double kilogramUFunte(double a) {
 		return 0;
 	}
-	
+
 	public double funteUKilograme(double a) {
 		return a / 2.2046;
 	}
-	
+
 	public double cmUInce(double a) {
 		return a / 2.54;
 	}
-	
+
 	public double inceUCm(double a) {
 		return 0;
 	}
-	
+
 	public double celzijusiUFarenhajte(double a) {
 		return 0;
 	}
-	
+
 	public double farnehajtiUCelzijuse(double a) {
-		return 0;
+		return (a - 32) * 5 / 9;
 	}
 
 }
