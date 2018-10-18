92eae0df851984898d31447df9d559a3f0438ce1
Nefisa Brčaninović <nefisa.h@hotmail.com>
2018-10-18 13:40:29 +0200

    Metode postavljene na static

--- src/Konverter.java
+++ src/Konverter.java
@@ -1,27 +1,27 @@
 
 public class Konverter {
 
-	public double kilogramUFunte(double a) {
+	public static double kilogramUFunte(double a) {
 		return 0;
 	}
 
-	public double funteUKilograme(double a) {
+	public static double funteUKilograme(double a) {
 		return a / 2.2046;
 	}
 
-	public double cmUInce(double a) {
+	public static double cmUInce(double a) {
 		return a / 2.54;
 	}
 
-	public double inceUCm(double a) {
-		return a*2.55;
+	public static double inceUCm(double a) {
+		return a * 2.55;
 	}
 
-	public double celzijusiUFarenhajte(double a) {
+	public static double celzijusiUFarenhajte(double a) {
 		return a * 1.8 + 32;
 	}
 
-	public double farnehajtiUCelzijuse(double a) {
+	public static double farnehajtiUCelzijuse(double a) {
 		return (a - 32) * 5 / 9;
 	}
 
